d94f52361f4b997e9eb7ed43afed082a59167953
Agnes <agnes.rosa@hotmail.com>
2022-05-23 15:20:36 -0300

    feat/fix: driver delete service / drive interface and controller fixes

---
+++ src/services/drivers/driver.delete.service.ts
@@ -0,0 +1,21 @@
+import { AppDataSource } from "../../data-source"
+import Driver from "../../models/Driver"
+import AppError from "../../errors/AppError";
+
+const deleteDriverService = async (name: string) => {
+    const driverRepository = AppDataSource.getRepository(Driver)
+
+    const drivers = await driverRepository.find()
+
+    const foundDriver = drivers.find(driver => driver.name === name)
+
+    if (!foundDriver) {
+        throw new AppError(`We could not find a driver under the name ${name}`)
+    }
+
+    await driverRepository.delete(foundDriver!.id)
+
+    return true
+}
+
+export default deleteDriverService
--- src/services/drivers/driver.update.service.ts
+++ src/services/drivers/driver.update.service.ts
@@ -2,15 +2,15 @@ import { AppDataSource } from "../../data-source";
 import AppError from "../../errors/AppError";
 import Driver from "../../models/Driver";
 
-const updateDriverService = async (name: string, telephone: string) => {
+const updateDriverService = async (driverId: string, name: string, telephone: string) => {
     const driverRepository = AppDataSource.getRepository(Driver)
 
     const drivers = await driverRepository.find()
 
-    const foundDriver = drivers.find(driver => driver.name === name)
+    const foundDriver = drivers.find(driver => driver.id === driverId)
 
     if (!foundDriver) {
-        throw new AppError(`We could not find a driver under the name ${name}`)
+        throw new AppError(`We could not find a driver under the id ${driverId}`)
     }
 
     await driverRepository.update(foundDriver!.id, { 
